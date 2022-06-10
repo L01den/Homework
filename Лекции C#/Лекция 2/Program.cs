430f22476451d9a5519349cadc0d7daf4b3912e4
Daria <procionestrisce@yandex.ru>
2022-06-10 20:30:43 +0300

    Exam_9

--- Лекции C#/Лекция 2/Program.cs
+++ Лекции C#/Лекция 2/Program.cs
@@ -1,4 +1,6 @@
-﻿int Max(int arg1, int arg2, int arg3)
+﻿//Задача 9
+
+int Max(int arg1, int arg2, int arg3)
 {
     int result = arg1;
     if (arg2 > result) result = arg2;
@@ -6,23 +8,17 @@
     return result;
 }
 
-int a1 = 1;
-int b1 = 2;
-int c1 = 344;
+int [] array = {12, 34, 45, 13, 56, 18, 16, 28, 46};
 
-int a2 = 4;
-int b2 = 51;
-int c2 = 6;
 
-int a3 = 7;
-int b3 = 8;
-int c3 = 9;
 
-/* int maxim1 = Max(a1, b1, c1);
-int maxim2 = Max(a2, b2, c2);
-int maxim3 = Max(a3, b3, c3);
-int Maxi = Max(maxim1, maxim2, maxim3); */
+int result = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8]));
 
-int Maxi = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+Console.Write(result);
 
-Console.Write(Maxi);
+
+
+
